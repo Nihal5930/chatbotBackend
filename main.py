c4fe4025d9ac964adca2f75b5dcb8ed66fb61009
nihal5930 <nihal5930@gmail.com>
2025-04-16 22:00:01 +0530

    first-1

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 # Allow React frontend to connect
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # React dev server
+    allow_origins=["http://13.127.57.5:5177"],  # React dev server
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
